e7bffb55fdd2d0472f2eaf5f14c5fc013f5a0b41
simpleframework <cknet@126.com>
2013-11-15 11:43:03 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/workflow/engine/impl/WorkflowContext.java
+++ src/main/java/net/simpleframework/workflow/engine/impl/WorkflowContext.java
@@ -33,7 +33,7 @@ import net.simpleframework.workflow.schema.AbstractTaskNode;
  * Licensed under the Apache License, Version 2.0
  * 
  * @author 陈侃(cknet@126.com, 13910090885)
- *         http://code.google.com/p/simpleframework/
+ *         https://github.com/simpleframework
  *         http://www.simpleframework.net
  */
 public abstract class WorkflowContext extends AbstractDbModuleContext implements IWorkflowContext {
